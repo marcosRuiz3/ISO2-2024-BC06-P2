711c38f1b18720b11d5705d5b8ec3f515ccb8f72
Marcos <Marcos.Ruiz3@alu.uclm.es>
2024-12-17 23:30:08 +0100

    Actualizando metodo

--- BC06-P2/src/main/java/esi/uclm/BC06_P2/Determinar_Cuenta.java
+++ BC06-P2/src/main/java/esi/uclm/BC06_P2/Determinar_Cuenta.java
@@ -3,26 +3,32 @@ package esi.uclm.BC06_P2;
 public class Determinar_Cuenta {
 	
 	public String determinar_cuenta(Cliente cliente){
-		
+		String salida= "Condicion no válida para determinar un tipo de cuenta";
 		if(cliente.getEdadd() < 18 && cliente.isEstudiaa() && cliente.isViveconPadres()) {
-			return "Cuenta Confort";
+			salida= "Cuenta Confort";
+			return salida;
 		} else if (cliente.getEdadd() < 25 && cliente.isEstudiaa() && !cliente.isViveconPadres()) {
-			return "Cuenta_vamos_que_tu_Puedes";	
+			salida= "Cuenta_vamos_que_tu_Puedes";	
+			return salida;
 		} else if(cliente.getEdadd() >= 18 && !cliente.isEstudiaa()) {
 			if (cliente.getEdadd() <= 25) {
 				if (cliente.isViveconPadres()) {
-					return "Cuenta_Ahorra_ahora_que_Puedes";
+					salida="Cuenta_Ahorra_ahora_que_Puedes";
+					return salida;
 				} else {
-					return "Cuenta_saltando_del_Nido";
+					salida= "Cuenta_saltando_del_Nido";
+					return salida;
 			    }
 			} else { 
 				if (cliente.isViveconPadres()) {
-					return "Cuenta_Independizate_que_va_siendo_hora";	
+					salida="Cuenta_Independizate_que_va_siendo_hora";
+					return salida;	
 				} else {
-					return "Cuenta_Bienvenido_Vida_Adulta";
+					salida="Cuenta_Bienvenido_Vida_Adulta";
+					return salida;
 				}
 			}
 		}
-		return "Condicion no válida para determinar un tipo de cuenta";
+		return salida;
 	}
 }
